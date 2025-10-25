f3eb8cca48e2bbb4221af6ab665461a06f3b2d7d
Kalshidex <ryzefn247@gmail.com>
2025-10-25 16:31:19 +0400

    Add files via upload

---
+++ scripts/portfolio/delete_order.sh
@@ -0,0 +1,4 @@
+curl --request DELETE \
+     --url https://demo-api.kalshi.co/trade-api/v2/portfolio/orders/69131f24-fd49-4697-a581-3e31aa46d6bb \
+     --header 'Authorization:{token}' \
+     --header 'accept: application/json'
